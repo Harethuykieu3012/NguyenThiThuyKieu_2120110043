b6746e6125d2b1f40996fd991fb470c38580c302
Harethuykieu3012 <NguyenThiThuyKieu02032002@gmail.com>
2023-04-13 12:51:48 +0700

    13/4

--- app/Http/Controllers/backend/CategoryController.php
+++ app/Http/Controllers/backend/CategoryController.php
@@ -5,14 +5,15 @@ namespace App\Http\Controllers\backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
     // GET admin/cayegory, admin/category/index
     public function index()
     {
-        $list_category = Category::get();
-        return view('backend.category.index',compact('list_category'));
+        $list_category = Category::where('status', '!=', 0)->get();
+        return view('backend.category.index', compact('list_category'));
     }
 
     /**
@@ -20,7 +21,15 @@ class CategoryController extends Controller
      */
     public function create()
     {
-       echo('đã thêm');
+        $list_category = Category::where('status', '!=', 0)->get();
+        $html_parent_id='';
+        $html_sort_order = '';
+        foreach($list_category as $item)
+        {
+            $html_parent_id .= '<option value="' . $item->id. '">' . $item->name . '</option>';
+            $html_sort_order .= '<option value="' . $item->sort_order . '">Sau:' . $item->name . '</option>';
+        }
+        return view('backend.category.create',compact('html_parent_id', 'html_sort_order'));
     }
 
     /**
@@ -28,15 +37,30 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+       $category = new Category;
+       $category->name=$request->name;
+       $category->slug=Str::slug($category->name=$request->name,'-');
+       $category->metakey = $request->metakey;
+       $category->metadesc = $request->metadesc;
+       $category->parent_id = $request->parent_id;
+        $category->sort_order = $request->sort_order;
+        $category->status = $request->status;
+        $category->save();
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $category = Category::find($id);
+        if ($category == null) {
+            return redirect()->route('category.index')->with('message', ['type' => 'danger', 'msg'
+            => 'Mẫu tin không tồn tại!']);
+        }else{
+            //return view('backend.category.show',compat('category'));
+        }
+            
     }
 
     /**
@@ -61,5 +85,41 @@ class CategoryController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+    //get:adim/category/status/1
+    public function status($id)
+    {
+        $category = Category::find($id);
+        if ($category == null) {
+            return redirect()->route('category.index')->with('message', ['type' => 'danger', 'msg'
+            => 'Mẫu tin không tồn tại!']);
+
+            //chuyen huong bao loi
+        } else {
+            $category->status = ($category->status == 1) ? 2 : 1;
+            $category->updated_at = date('Y-m-d H:i:s');
+            $category->updated_by = 1;
+            $category->save();
+            return redirect()->route('category.index')->with('message', ['type' => 'success', 'msg'
+            => 'Thay đổi trang thái thành công']);
+        }
+    }
+    //get:adim/category/delete/1
+    public function delete($id)
+    {
+        $category = Category::find($id);
+        if ($category == null) {
+            return redirect()->route('category.index')->with('message', ['type' => 'danger', 'msg'
+            => 'Mẫu tin không tồn tại!']);
+
+            //chuyen huong bao loi
+        } else {
+            $category->status = 0;
+            $category->updated_at = date('Y-m-d H:i:s');
+            $category->updated_by = 1;
+            $category->save();
+            return redirect()->route('category.index')->with('message', ['type' => 'success', 'msg'
+            => 'Xóa vào thùng rác']);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', [SiteController::class, 'index'])->name('site.home');
 Route::prefix('admin')->group(function(){
    Route::get('/',[DashboardController::class,'index'])->name('admin.dashboard');
    Route::resource('brand', BrandController::class);
+   // Category
    Route::resource('category', CategoryController::class);
+   Route::prefix('admin')->group(function () {
+         Route::get('status/{category}', [CategoryController::class, 'status'])->name('category.status');
+         Route::get('delete/{category}', [CategoryController::class, 'delete'])->name('category.delete');
+   });
    Route::resource('product', ProductController::class);
 });
